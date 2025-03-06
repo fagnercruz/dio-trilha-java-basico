3b6b2428f68d7f69195ec3c1cfa87902f6b714d4
Fagner Cruz <fagner@sti.ufpb.br>
2025-03-06 13:38:43 -0300

    Implementando as regras de negócio propostas no desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Banco.");
+         int numero;
+         String agencia;
+         String nomeCliente;
+         double saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da agência: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Agora informe a agência: ");
+        agencia = sc.nextLine();
+
+        System.out.print("Informe agora o seu nome completo: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.print("Agora informe o saldo da sua conta: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!\n" +
+                "Sua agência é " + agencia + ", conta " + numero + " e seu " +
+                "saldo " + saldo + " já está disponível para saque.");
+
+        sc.close();
+
     }
 }
